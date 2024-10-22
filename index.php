a293c81d5a697df40aa56d0c8d60d0065fba8826
Bipasna Dulal <bipasna.dulal75@gmail.com>
2024-10-22 22:00:24 +0545

    created function.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+  include("database/connect.php");
+  include('functions/functions.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -90,29 +95,7 @@
          <div class="row">
           <!-- fetching products -->
           <?php
-            include("database/connect.php");
-            $select_query="Select * from `products` order by rand() limit 0,8";
-            $result_query=mysqli_query($conn, $select_query);
-            while($row=mysqli_fetch_assoc($result_query)){
-              $product_id=$row['product_id'];
-              $product_title=$row['product_title'];
-              $product_description=$row['product_description'];
-              $product_keywords=$row['product_keywords'];
-              $product_image=$row['product_image'];
-              $product_price=$row['product_price'];
-              $category_id=$row['category_id'];
-              echo "<div class='col-md-3'>
-          <div class='card'>
-            <img src='./admin_area/product_images/$product_image' class='card-img-top' alt='$product_title'>
-            <div class='card-body'>
-              <h5 class='card-title'>$product_title</h5>
-              <p class='card-text'>$product_description</p>
-              <a href='#' class='btn custom-button'>Add to Cart</a>
-              <a href='#' class='btn custom-view-button'>View More</a>
-            </div>
-          </div>
-          </div>";
-            }
+          getProducts();
           ?>
         
 
